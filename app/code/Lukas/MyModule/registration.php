05a1577506a708b9ac143c195ab3b5b8219103ac
Lukas <l.kezevicius@gmail.com>
2020-06-02 09:37:12 +0300

    test-commit

--- app/code/Lukas/MyModule/registration.php
+++ app/code/Lukas/MyModule/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Lukas_MyModuleContact',
+    'Lukas_MyModule',
     __DIR__
 );
