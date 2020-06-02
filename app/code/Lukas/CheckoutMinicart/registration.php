200056322cd8945d27181849b76239dfa51a03b5
Lukas <l.kezevicius@gmail.com>
2020-06-02 11:45:05 +0300

    test minicart

--- app/code/Lukas/CheckoutMinicart/registration.php
+++ app/code/Lukas/CheckoutMinicart/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Lukas_MyModule',
+    'Lukas_CheckoutMinicart',
     __DIR__
 );
