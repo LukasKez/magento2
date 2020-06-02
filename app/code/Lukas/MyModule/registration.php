4607262b8b560c3357daf6e1a9e97e81bb5a35a4
Lukas <l.kezevicius@gmail.com>
2020-06-02 17:54:40 +0300

    minicart checkout button removed

--- app/code/Lukas/MyModule/registration.php
+++ app/code/Lukas/MyModule/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Lukas_CheckoutMinicart',
+    'Lukas_MyModule',
     __DIR__
 );
